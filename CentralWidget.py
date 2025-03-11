14e47ba4cb61ea34582bcf1d2fe8fa0774051810
chey00 <115455604+chey00@users.noreply.github.com>
2025-03-11 10:16:59 +0100

    Quelltext für die Abschlussprüfung

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,8 +1,41 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCharts import QChartView, QChart, QDateTimeAxis, QValueAxis, QSplineSeries
+from PyQt6.QtCore import Qt, QDateTime, pyqtSlot
 
 
-class CentralWidget(QWidget):
+class CentralWidget(QChartView):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
 
-        # Lets go...
+        self.__value_start = QSplineSeries()
+        self.__value_high = QSplineSeries()
+        self.__value_low = QSplineSeries()
+        self.__value_end = QSplineSeries()
+
+        # Requirement 2.6
+
+    @pyqtSlot(str)
+    def append_values(self, line):
+        cells = line.split(";")
+
+        datetime = QDateTime.fromString(cells[0], "dd.MM.yyyy").toMSecsSinceEpoch()
+
+        start = self.currency_to_float(cells[1])
+        self.__value_start.append(datetime, start)
+
+        high = self.currency_to_float(cells[2])
+        self.__value_high.append(datetime, high)
+
+        low = self.currency_to_float(cells[3])
+        self.__value_low.append(datetime, low)
+
+        end = self.currency_to_float(cells[4])
+        self.__value_end.append(datetime, end)
+
+    def currency_to_float(self, string):
+        tokens = string.split(" ")
+
+        float_value = tokens[0].replace(",", ".")
+
+        tmp = float(float_value)
+
+        return tmp
--- MainWindow.py
+++ MainWindow.py
@@ -1,14 +1,50 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtCore import pyqtSlot, QFile, QIODevice, QTextStream, pyqtSignal
+from PyQt6.QtWidgets import QMainWindow, QMenuBar, QFileDialog, QMessageBox, QProgressBar, QStatusBar
 
 from CentralWidget import CentralWidget
 
 
 class MainWindow(QMainWindow):
+    new_line = pyqtSignal(str)
+
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
         central_widget = CentralWidget(self)
+        self.new_line.connect(central_widget.append_values)
 
         self.setCentralWidget(central_widget)
 
-        self.setWindowTitle("Change me!")
+        self.setWindowTitle("Abschlussprüfung 2024/25")
+        self.setBaseSize(800, 600)
+        self.resize(800, 600)
+
+        status_bar = QStatusBar()
+
+        self.__progress_bar = QProgressBar()
+
+        status_bar.addWidget(self.__progress_bar)
+
+        self.setStatusBar(status_bar)
+
+        # Requirement 2.1
+
+    @pyqtSlot()
+    def open_file(self):
+        # Requirement 2.2
+        pass
+
+        # Requirement 2.3
+
+        # Requirement 2.4
+
+        # Requirement 2.5
+
+            # length = len(lines) - 1
+            #
+            # self.__progress_bar.setRange(0, length)
+            #
+            # for i in range(length):
+            #     self.new_line.emit(lines[i])
+            #
+            #     self.__progress_bar.setValue(i + 1)
